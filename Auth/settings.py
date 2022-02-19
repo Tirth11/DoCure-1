642fcda7a92c7f61d2e83b1b39e6e39f3ce5a080
unknown <anushkadarade@gmail.com>
2022-02-19 17:31:40 +0530

    update

--- Auth/settings.py
+++ Auth/settings.py
@@ -136,11 +136,11 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-from django.contrib.messages import constants as messages
-MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
+# from django.contrib.messages import constants as messages
+# MESSAGE_TAGS = {
+#         messages.DEBUG: 'alert-secondary',
+#         messages.INFO: 'alert-info',
+#         messages.SUCCESS: 'alert-success',
+#         messages.WARNING: 'alert-warning',
+#         messages.ERROR: 'alert-danger',
+#  }
