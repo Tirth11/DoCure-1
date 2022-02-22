4c623af72a57932556e29825bb3f9956b9897049
unknown <anushkadarade@gmail.com>
2022-02-22 23:49:51 +0530

    6

--- Auth/urls.py
+++ Auth/urls.py
@@ -13,15 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 
 from django.views.static import serve
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('home.urls')),
-    url(r'^media/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
